48c8cfd94aaadfeba9cec5119d68df9a36aa1720
Dolph Xia <dolphxia@gmail.com>
2020-05-23 16:22:12 +0800

    change image link to cover_art_url

--- rss_element_tree.py
+++ rss_element_tree.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 import xml.etree.ElementTree as ET
 import json
@@ -11,14 +10,14 @@ stuff = []
 
 for child in myroot.findall('channel/item'):
     stuff.append(child)
-    stuff = stuff[::-1]
+stuff = stuff[::-1]
 for child in stuff:
     title=child.find('title').text
     link=child.find('enclosure').attrib
     image=child.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}image').attrib
     url=link['url']
     image_link=image['href']
-    dicd={"name":title,"url":url,"image_link":image_link}
+    dicd={"name":title,"url":url,"cover_art_url":image_link}
     rsssongs.append(dicd)
    
     count += 1
@@ -33,39 +32,3 @@ with open("songs.json", "w") as write_file:
 
 
 
-=======
-import requests
-import xml.etree.ElementTree as ET
-import json
-mytree = ET.parse('rss.xml')
-myroot = mytree.getroot()
-rsssongs=[]
-count = 1
-showname = "ANS"
-stuff = []
-
-for child in myroot.findall('channel/item'):
-    stuff.append(child)
-    stuff = stuff[::-1]
-for child in stuff:
-    title=child.find('title').text
-    link=child.find('enclosure').attrib
-    image=child.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}image').attrib
-    url=link['url']
-    image_link=image['href']
-    dicd={"name":title,"url":url,"image_link":image_link}
-    rsssongs.append(dicd)
-   
-    count += 1
-
-songs={"songs":rsssongs}
-
-with open("songs.json", "w") as write_file:
-    json.dump(songs, write_file)
-
-
-
-
-
-
->>>>>>> b1bb34cc1aad500a5691009ce0bd5b1ed642fd1e
